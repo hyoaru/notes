39feef8ac50979c52a3c7a213e32d191b2f92d28
hyoaru <jjcabreraaaa@gmail.com>
2021-09-18 15:52:53 +0800

    [add] fromjson and tojson property

--- lib/model/notes_model.dart
+++ lib/model/notes_model.dart
@@ -1,6 +1,15 @@
 final String tableNotes = 'notes';
 
 class NoteFields {
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    dateCreated
+  ];
+
   static final String id = '_id';
   static final String isImportant = 'isImportant';
   static final String number = 'number';
@@ -25,4 +34,39 @@ class Note {
   final String title;
   final String description;
   final DateTime dateCreated;
+
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? dateCreated,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        dateCreated: dateCreated ?? this.dateCreated,
+      );
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        isImportant: json[NoteFields.isImportant] == 1,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        dateCreated: DateTime.parse(json[NoteFields.dateCreated] as String),
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.title: title,
+        NoteFields.number: number,
+        NoteFields.description: description,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.dateCreated: dateCreated.toIso8601String(),
+      };
 }
