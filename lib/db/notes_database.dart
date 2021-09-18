eb8106c4ef3d1d24908598f02c1a90e1a94e90b8
hyoaru <jjcabreraaaa@gmail.com>
2021-09-18 15:49:46 +0800

    [add] create and read functionality

--- lib/db/notes_database.dart
+++ lib/db/notes_database.dart
@@ -1,6 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:notes_sqflite/model/notes.dart';
+import 'package:notes_sqflite/model/notes_model.dart';
 
 class NotesDatabase {
   static final NotesDatabase instance = NotesDatabase._init();
@@ -38,6 +38,31 @@ class NotesDatabase {
       ${NoteFields.dateCreated} $textType,
     )
     ''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+
+    final id = await db.insert(tableNotes, note.toJson());
+
+    return note.copy(id: id);
+  }
+
+  Future<Note> readNote(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableNotes,
+      columns: NoteFields.values,
+      where: '${NoteFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isEmpty) {
+      return Note.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
   }
 
   Future close() async {
